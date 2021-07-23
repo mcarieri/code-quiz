1e99d0742433e52c27736fbdfc2f7df95fef4982
Matthew Carieri <matthewcarieri@Matthews-MacBook-Pro-6.local>
2021-07-22 19:58:58 -0700

    script updated with notes and skeleton

---
+++ assets/script.js
@@ -0,0 +1,41 @@
+var timer = 0
+//high score button
+//query.Selectors: start button, high score button, back button, submit high score button, each question answer button, clear high score button, timer, score name submission form, high score table, go back button  
+
+//start button
+//load next question function
+//timer
+//function to remove time
+//feedback for correct/incorrect answers
+//html elements of question and answers
+//high score (local storage)
+//high score button (linked to html high score screen)
+//write question text in JS
+//build question/body template html
+//correct answer key
+
+var quizQuestion = [
+    {
+        question: "Question 1: What is code?",
+        answerA: "food",
+        answerB: "air",
+        answerC: "language",
+        answerD: "animal",
+        correctAnswer: "language"
+    },
+    
+    {
+        question: "Question 1: What is code?",
+        answerA: "food",
+        answerB: "air",
+        answerC: "language",
+        answerD: "animal",
+        correctAnswer: "language"
+    }
+]
+//does correctAnswer == user submitted answer
+
+
+
+
+//nested objects (answer object within q object)
